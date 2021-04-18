25e1da3d487cdebc8bf71fc457a1ae5382f46554
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-04-18 04:06:00 +0200

    Updates

---
+++ precache-manifest.0ee50db3f78f4f03c61e421186d33db6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272922c95b0c8255b3f9891942dad89c",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "739b5103262856a8a8fa",
+    "url": "/counter-app/static/css/main.13b8e13a.chunk.css"
+  },
+  {
+    "revision": "ef9edc3016fb83126c32",
+    "url": "/counter-app/static/js/2.938dc34d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/counter-app/static/js/2.938dc34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739b5103262856a8a8fa",
+    "url": "/counter-app/static/js/main.fabdfecb.chunk.js"
+  },
+  {
+    "revision": "8d3c91def09e322d68af",
+    "url": "/counter-app/static/js/runtime-main.7c4e8537.js"
+  },
+  {
+    "revision": "6191f4360767f10e91bc867f045b9ed7",
+    "url": "/counter-app/static/media/shopping-cart-icon.6191f436.webp"
+  }
+]);
